470225e6895f289d3f718df2fb607bbfef5fbdde
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2022-11-04 20:27:23 -0300

    desafio ok

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
-        {
-            Numero = numero;
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {      
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia1 = new Nokia("2197600-9515", "Nokia 350", "454563.659656", 2);
+nokia1.Ligar();
+nokia1.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("/n");
+
+Iphone iphone1 = new Iphone("2197600-9515", "Iphone 18", "54545.6445", 8);
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Twitter");
